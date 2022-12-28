699ea3d85a6543769ac2a30c721bc80bc0ed4691
Roy-Wanyoike <roywanyoike328@gmail.com>
2022-12-28 18:43:59 +0300

    added readme

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : 'cold'">
+      <mq-layout mq="mobile">
+    <mobile-menu></mobile-menu>
+  </mq-layout>
+  <mq-layout mq="tablet+">
+    <desktop-menu></desktop-menu>
+  </mq-layout>
     <main>
       <div class="search-box">
         <input type="text" class="search-bar" placeholder="Search..." v-model="query" @keypress="fetchWeather" />
@@ -21,6 +27,8 @@
 </template>
 
 <script>
+
+ 
 export default {
   name: 'app',
   data() {
